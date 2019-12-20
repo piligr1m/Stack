7c923197a1356216fda5878c6caafe3e3152ee6c
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-20 23:47:25 +0300

    Update Zadanie2.h

--- include/Zadanie2.h
+++ include/Zadanie2.h
@@ -1,8 +1,10 @@
+#ifndef Zadanie2_h
+#define Zadanie2_h
 #include <iostream>
 #include <memory>
 
 template <typename T>
-class Stack1
+class Stack2
 {
 private:
     static const unsigned int st_capacity = 32; // reserved memory
@@ -11,17 +13,19 @@ private:
     std::unique_ptr<T> st_ptr;
 
 public:
-    Stack1();
-    Stack1(const Stack1<T>& stack) = delete;
-    Stack1(Stack1<T>&& stack);
-    ~Stack1() = default;
+    Stack2();
+    Stack2(const Stack2<T>& stack) = delete;
+    Stack2(Stack2<T>&& stack);
+    ~Stack2() = default;
 
-    Stack1<T>& operator=(const Stack1<T>& stack) = delete;
-    Stack1<T>& operator=(Stack1<T>&& stack);
+    Stack2<T>& operator=(const Stack2<T>& stack) = delete;
+    Stack2<T>& operator=(Stack2<T>&& stack);
 
-    void push(T&& value);
+    template <typename ... Args>
+    void push_emplace(Args&&... value);
     void push(const T& value);
-    void pop();
+    void push(T&& value);
+    T pop();
 
     bool isEmpty() const;
     bool isFull() const;
@@ -30,21 +34,21 @@ public:
 };
 
 template <typename T>
-Stack1<T>::Stack1() {
+Stack2<T>::Stack2() {
     this->capacity = this->st_capacity;
     this->size = 0;
     this->st_ptr.reset(new T [this->st_capacity]);
 }
 
 template <typename T>
-Stack1<T>::Stack1(Stack1<T>&& stack) {
+Stack2<T>::Stack2(Stack2<T>&& stack) {
     std::swap(this->capacity, stack.capacity);
     std::swap(this->size, stack.size);
     std::swap(this->st_ptr, stack.st_ptr);
 }
 
 template <typename T>
-Stack1<T>& Stack1<T>::operator=(Stack1<T>&& stack) {
+Stack2<T>& Stack2<T>::operator=(Stack2<T>&& stack) {
     if(std::move(stack) != this) {
         std::swap(this->capacity, stack.capacity);
         std::swap(this->size, stack.size);
@@ -55,30 +59,7 @@ Stack1<T>& Stack1<T>::operator=(Stack1<T>&& stack) {
 }
 
 template <typename T>
-void Stack1<T>::push(T&& value) {
-    this->size++;
-    if (this->size > this->capacity && !this->isFull()) {
-        this->capacity *= 2;
-
-        std::unique_ptr<T> new_st(new T [this->capacity]);
-
-        for(int i = 0; i < this->size - 1; i++) {
-            new_st.get()[i] = this->st_ptr.get()[i];
-        }
-        new_st.get()[this->size - 1] = std::move(value);
-
-        this->st_ptr.swap(new_st);
-    }
-    else if (this->isFull()) {
-        throw std::logic_error("|Stack1 OVERFLOW|");
-        this->size--;
-    }
-    else this->st_ptr.get()[this->size - 1] = std::move(value);
-
-}
-
-template <typename T>
-void Stack1<T>::push(const T& value) {
+void Stack2<T>::push(const T& value) {
     this->size++;
     if (this->size > this->capacity && !this->isFull()) {
         this->capacity *= 2;
@@ -93,7 +74,7 @@ void Stack1<T>::push(const T& value) {
         this->st_ptr.swap(new_st);
     }
     else if (this->isFull()) {
-        throw std::logic_error("|Stack1 OVERFLOW|");
+        throw std::logic_error("|Stack2 OVERFLOW|");
         this->size--;
     }
     else
@@ -101,31 +82,64 @@ void Stack1<T>::push(const T& value) {
 }
 
 template <typename T>
-void Stack1<T>::pop() {
+void Stack2<T>::push(T&& value) {
+    this->size++;
+    if (this->size > this->capacity && !this->isFull()) {
+        this->capacity *= 2;
+
+        std::unique_ptr<T> new_st(new T [this->capacity]);
+
+        for(int i = 0; i < this->size - 1; i++) {
+            new_st.get()[i] = this->st_ptr.get()[i];
+        }
+        new_st.get()[this->size - 1] = std::move(value);
+
+        this->st_ptr.swap(new_st);
+    }
+    else if (this->isFull()) {
+        throw std::logic_error("|Stack2 OVERFLOW|");
+        this->size--;
+    }
+    else this->st_ptr.get()[this->size - 1] = std::move(value);
+}
+
+template <typename T>
+template <typename ... Args>
+void Stack2<T>::push_emplace(Args&&... value) {
+    T args [] = {value...};
+
+    for(const auto& e: args) {
+        this->push(e);
+    }
+}
+
+template <typename T>
+T Stack2<T>::pop() {
     if(!this->isEmpty()) {
         this->size--;
     }
     else
-        throw std::logic_error("|Stack1 IS EMPTY|");
+        throw std::logic_error("|Stack2 IS EMPTY|");
+
 
 }
 
 template <typename T>
-bool Stack1<T>::isEmpty() const {
+bool Stack2<T>::isEmpty() const {
      return this->size == 0;
 }
 
 template <typename T>
-bool Stack1<T>::isFull() const {
-     return this->size > 1000;
+bool Stack2<T>::isFull() const {
+     return this->size > 65536;
 }
 
 template <typename T>
-const T& Stack1<T>::head() const{
+const T& Stack2<T>::head() const {
     if (!this->isEmpty())
         return this->st_ptr.get()[this->size - 1];
     else
-        throw std::logic_error("|Stack1 IS EMPTY|");
+        throw std::logic_error("|Stack2 IS EMPTY|");
 }
 
-#endif /* Zadanie1_h */
+#endif /* Zadanie2_h */
