80e9e5c7ab50dfdfa845378c0cd2e3699e7aac3e
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-27 02:08:05 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
-#include "Zadanie1.h"
-#include "Zadanie2.h"
+#include "Zadanie.h"
 
 int main(int argc, const char * argv[]) {
-    // Stack 1
-    std::cout << "STACK1<INT> ST1:\n";
-    Stack1<int> st1;
-    for (int i = 0; i < 10; i++) {
-        st1.push(i);
-    }
-    for (int i = 0; i < 10; i++) {
-        std::cout << st1.head() << std::endl;
-        st1.pop();
-    }
-
     // Stack 2
     std::cout << "\nSTACK2<INT> ST2:\n";
-    Stack2<int> st2;
+    Stack<int> st2;
     st2.push(1);
     st2.push(2);
     st2.push_emplace(3, 4, 5);
@@ -24,16 +12,5 @@ int main(int argc, const char * argv[]) {
         std::cout << st2.head() << std::endl;
         st2.pop();
     }
-
-    // Moving st1 to st3
-    std::cout << "\nSTACK1<INT> ST3 = ST1, ST2:\n";
-    for (int i = 0; i < 10; i++) {
-        st1.push(i);
-    }
-    Stack1<int> st3 = std::move(st1);
-    for (int i = 0; i < 10; i++) {
-        std::cout << st3.head() << std::endl;
-        st3.pop();
-    }
-
 }
+
