af6ddc1663da2a093c9607e40ec8baa60adcedef
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2019-12-30 16:02:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, const char * argv[]) {
     std::cout << "\nSTACK<INT> ST:\n";
-    Stack<int> st2;
-    st2.push(1);
-    st2.push(2);
-    st2.push_emplace(3, 4, 5);
+    Stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push_emplace(3, 4, 5);
     for (int i = 0; i < 5; i++) {
-        std::cout << st2.head() << std::endl;
-        st2.pop();
+        std::cout << st.head() << std::endl;
+        st.pop();
     }
 }
 
