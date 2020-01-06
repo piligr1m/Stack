e7e09b5b934f9aeb9dfa7a0f82f6e0ebcefb0aef
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2020-01-06 17:22:36 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,15 +1,30 @@
 #include "stack.h"
 #include <iostream>
 
+struct Student
+{
+    std::string Name;
+    int age;
+    int stipendia;
+    
+};
 int main(int argc, const char * argv[]) {
     std::cout << "Stack:" << std::endl;
     Stack<int> st;
     st.push(1);
     st.push(2);
-    st.push_emplace(3, 4, 5);
+    st.push_emplace(3);
+    st.push_emplace(4);
+    st.push_emplace(5);
+    Stack<Student> group;
+    group.push_emplace("Martynov", 19, 1111111);
+    
+    
     for (int i = 0; i < 5; i++) {
         std::cout << st.head() << std::endl;
         st.pop();
     }
+
+
 }
 
