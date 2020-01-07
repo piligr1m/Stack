5677584703e548ef4c753b8dd4f5d4bd0923b9ed
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2020-01-07 14:03:36 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,15 +3,19 @@
 
 struct Student
 {
-    int course;
+    std::string name;
     int age;
     int stipendia;
+    friend std::ostream& operator <<(std::ostream& out, Student group)
+    {
+       out<<group.name<<group.age<<group.stipendia;
+        return out;
+    }
 };
 int main(int argc, const char * argv[]) {
     std::cout << "Stack:" << std::endl;
     Stack<Student> group;
-    group.push_emplace(2, 19, 1111111);
-    group.head();
-    group.pop();
+    group.push_emplace("aaa", 19, 1111111);
+    std::cout<<group.head();
 }
 
