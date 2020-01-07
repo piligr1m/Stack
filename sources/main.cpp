d7b6909899efd8a6bfc69bff1c78cd2530c751bf
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2020-01-07 12:49:53 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, const char * argv[]) {
     std::cout << "Stack:" << std::endl;
     Stack<Student> group;
     group.push_emplace("aa", 19, 1111111);
-    std::cout<<group.head();
+    group.head();
     group.pop();
 }
 
