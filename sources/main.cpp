02d671e8ea091a2ce41d72fafd8625f8b01dc156
Martynov Stas <47531255+piligr1m@users.noreply.github.com>
2020-01-07 13:35:07 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,14 +3,14 @@
 
 struct Student
 {
-    std::string Name;
+    int course;
     int age;
     int stipendia;
 };
 int main(int argc, const char * argv[]) {
     std::cout << "Stack:" << std::endl;
     Stack<Student> group;
-    group.push_emplace("aa", 19, 1111111);
+    group.push_emplace(2, 19, 1111111);
     group.head();
     group.pop();
 }
